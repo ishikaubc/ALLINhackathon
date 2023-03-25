0cce5769f980e682c6f3c55422e154736aac7fdb
Aditya Shah <meaditya2003@gmail.com>
2023-03-25 18:00:31 -0400

    First Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void output(int id, String result){
+        View view = findViewById(id);
+        TextView textView = (TextView) view;
+        textView.setText(result);
+    }
+
+    private String input(int id){
+        View view = findViewById(id);
+        EditText editText = (EditText) view;
+        String str = editText.getText().toString();
+        return str;
+    }
+
+
+
+}
