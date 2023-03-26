3801681741a781004da7da8976a3b0d9e8f4fb0a
Aditya Shah <meaditya2003@gmail.com>
2023-03-26 04:28:24 -0400

    Second Commit, with login and sign up

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -28,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         return str;
     }
 
-
+    public void onStart(View view){
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
 
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,4 +15,23 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+
+    private void output(int id, String result){
+        View view = findViewById(id);
+        TextView textView = (TextView) view;
+        textView.setText(result);
+    }
+
+    private String input(int id){
+        View view = findViewById(id);
+        EditText editText = (EditText) view;
+        String str = editText.getText().toString();
+        return str;
+    }
+
+    public void onSignUp(View view){
+        Intent i = new Intent(this, MainActivity3.class);
+        startActivity(i);
+    }
+
 }
