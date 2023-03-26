e68576ed884ba1684fb1a3c4c83979a017a9ba92
Aditya Shah <meaditya2003@gmail.com>
2023-03-26 06:45:14 -0400

    Third commit, hompage added

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -11,4 +15,23 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
     }
+
+    private void output(int id, String result){
+        View view = findViewById(id);
+        TextView textView = (TextView) view;
+        textView.setText(result);
+    }
+
+    private String input(int id){
+        View view = findViewById(id);
+        EditText editText = (EditText) view;
+        String str = editText.getText().toString();
+        return str;
+    }
+
+    public void onSignUp(View view){
+        Intent i = new Intent(this, MainActivity4.class);
+        startActivity(i);
+    }
+
 }
